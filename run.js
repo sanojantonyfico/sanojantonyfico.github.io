97a515f8872ec02c693117c3bff83567c1695bb3
Sanoj Antony <sanojantony@fico.com>
2017-09-20 15:32:39 -0700

    Adding first set of files

---
+++ run.js
@@ -0,0 +1,24 @@
+/**
+ * File Name: runjs.js
+ *
+ * This material is the confidential, unpublished property of
+ * Fair Isaac Corporation. Receipt or possession of this material does not
+ * convey rights to divulge, reproduce, use, or allow others to use it
+ * without the specific written authorization of Fair Isaac Corporation and
+ * use must conform strictly to the license agreement.
+ *
+ * Copyright (c) 2010-2017 Fair Isaac Corporation. All Rights Reserved.
+ */
+
+function printToConsole() {
+    console.log("Executed runjs");
+}
+
+function throwalert() {
+    alert("Throwing alert");
+}
+
+
+printToConsole();
+throwalert();
+
